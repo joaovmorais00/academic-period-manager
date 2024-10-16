cc4f6db2c5dd8c621f0d8783bd5c56d4e7426fff
João Vitor Morais <joao.morais00@hotmail.com>
2024-10-16 00:55:05 -0300

    Adding full Calendar

--- src/app/(home)/scheduler/page.tsx
+++ src/app/(home)/scheduler/page.tsx
@@ -1,5 +1,29 @@
+"use client";
 import TemplatePage from "@/components/common/TemplatePage/TemplatePage";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import ptBrLocale from "@fullcalendar/core/locales/pt-br";
+const events = [{ title: "Meeting", start: new Date() }];
 
 export default function page() {
-  return <TemplatePage title="Agenda">Agenda</TemplatePage>;
+  return (
+    <TemplatePage title="Agenda">
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={events}
+        eventContent={renderEventContent}
+        locale={ptBrLocale}
+      />
+    </TemplatePage>
+  );
+}
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
 }
